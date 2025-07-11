32907baf0b7497433e4dc7e8df0b3091ca6a6f34
Damiano Donati <damiano.donati@gmail.com>
2025-07-11 21:59:00 +0200

    Update jellyfin.sh

--- ct/jellyfin.sh
+++ ct/jellyfin.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(./misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/damdo/Proxmox/main/misc/build.func)
 # Copyright (c) 2021-2024 damdo
 # Author: damdo (damdoster)
 # License: MIT
